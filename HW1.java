3709b93a3650dd351c0e51896dacd5c8092db735
Eren <eren.gurbuz@ug.bilkent.edu.tr>
2025-02-05 19:49:47 +0300

    Step e
    Method for calculating the sum of even and odd added

--- HW1.java
+++ HW1.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class HW1
 {
@@ -26,9 +27,23 @@ public class HW1
 
     }
 
-    // Eren Gürbüz
-    public static void taskE()
+    /*
+     * author : Eren Gürbüz
+     * Date: 05.02.2025
+     * Method for calculating and printing the sum of elements at even and odd indexes
+     */
+    public static void SumOfElements ( int [] nums)
     {
-
+        int oddSum = 0 , evenSum = 0 ;
+        for ( int i = 0 ; i < nums.length ; i++) {
+            if ( i % 2 == 0) {
+                evenSum +=  nums [i];
+            }
+            else {
+                oddSum += nums [i] ;
+            }
+        } 
+        System.out.println("Sum of the elements at even indexes: " + evenSum);
+        System.out.println("Sum of the elements at odd indexes: " + oddSum);
     }
 }
