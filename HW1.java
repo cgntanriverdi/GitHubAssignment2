99fce33a29a0502d1d9dc3fda9defc9768c377db
Kaan Uz <kaan.uz@ug.bilkent.edu.tr>
2025-02-05 14:49:32 +0300

    Add files via upload

--- HW1.java
+++ HW1.java
@@ -1,13 +1,34 @@
 
 public class HW1
 {
+    // Task B (Main) - Baran Elkansu
     public static void main(String [] args)
     {
 
     }
 
-    public static void d()
+    // Ali Batu Sarıca
+    public static void taskA()
     {
         
+    }
+
+
+    // Çağan Tanrıverdi
+    public static void taskC()
+    {
+        
+    }
+
+    // Kaan Uz
+    public static void taskD()
+    {
+
+    }
+
+    // Eren Gürbüz
+    public static void taskE()
+    {
+
     }
 }
